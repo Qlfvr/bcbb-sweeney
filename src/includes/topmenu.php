b3e7e7915edc31e13d063d6becb9932c5ffed07f
Quentin Lefèvre <lefevre.quentin@gmail.com>
2020-03-03 15:28:54 +0100

    add simple top menu

---
+++ src/includes/topmenu.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="/" class="navbar-brand">Sweeney</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar7">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse justify-content-stretch" id="navbar7">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link btn btn-outline-secondary" href="#">Sign Up</a>
+            </li>
+
+            &nbsp;
+
+            <li class="nav-item">
+                <a class="nav-link nav-link btn btn-primary text-white" href="#">Sign In</a>
+            </li>
+        </ul>
+    </div>
+</nav>
