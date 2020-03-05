39c6848b5e7005e95798b8be09bc2ac521a19931
Guillaume-Boeur <guillaume.boeur@gmail.com>
2020-03-05 13:51:52 +0100

    inscription_ajout new user

--- src/loggin.php
+++ src/loggin.php
@@ -6,22 +6,48 @@
     <title>Sign in or sign up</title>
 </head>
 <body>
-    <form action="post" action="">
+
+<!-- Sign In -->
+
+    <form method="post" action="loggin.php">
         <p>Create an account :</p>
-       <!-- <input type="text" name="firstname" id="firstname" placeholder="Firstname" required autofocus/><br/> -->
-       <!-- <input type="text" name="lastname" id="lastname" placeholder="Lastname" required/><br/> -->
-       <!-- <input type="date" name="birthday" id="birthday" placeholder="birthday" required/><br/> -->
-       <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo" required/><br/>
-        <input type="email" name="email" id="email" placeholder="e-mail" required/><br/>
+        <input type="text" name="nickname" id="nickname" placeholder="Nickname" required/><br/>
+        <input type="email" name="email" id="email" placeholder="E-mail" required/><br/>
         <input type="password" name="password" id="password" placeholder="Password" required/><br/>
+        <!-- <input type="password" name="password_confirm" id="password_confirm" placeholder="Confirmation" required/></br> -->
         <input type="submit" name="submit" id="submit" value="Sign up"/>
     </form>
-    <form action="post" action="">
-        <p>Connextion :</p>
-        <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo" required/><br/>
+
+   <?php
+   // Connection à la base de donnée
+   try {
+       $bdd = new PDO('mysql:host=mysql;dbname=bcbb;charset=utf8', 'root', 'root');
+   } catch (Exception $e) {
+       die('Error : ' . $e->getMessage());
+   }
+
+    // Ajout User
+  $new_user = $bdd->prepare('INSERT INTO users (nickname, email, password) VALUES (:nickname, :email, :password)');
+   if(isset($_POST['submit'])){ // Si il y a envoie du formulaire, execute l'ajout des champs dans la BDD
+       $new_user->execute(array(
+           'nickname' => $_POST['nickname'],
+           'email' => $_POST['email'],
+           'password' => $_POST['password']
+       ));
+       echo 'Welcome, new user';
+
+   }
+   ?>
+
+<!-- Sign Up -->
+
+    <form method="post" action="index.html">
+        <p>Connection :</p>
+        <input type="text" name="nickname" id="nickname" placeholder="Nickname" required/><br/>
         <input type="password" name="password" id="password" placeholder="Password" required/><br/>
         <input type="submit" name="submit" id="submit" value="Sign in"/>
     </form>
 
+    
 </body>
 </html>
