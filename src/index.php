b3d3cc5a1c67fe80840c30b488f2247df17b913f
Guillaume-Boeur <guillaume.boeur@gmail.com>
2020-03-06 12:22:47 +0100

    Sign in and Sign up basic runing

--- src/index.php
+++ src/index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html class="no-js" lang="en">
 
@@ -13,8 +14,7 @@
 </head>
 
 <body>
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
