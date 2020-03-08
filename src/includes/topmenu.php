8c0485fbcff56a5ab883bb42c9771c3b54d4db31
Quentin Lefèvre <lefevre.quentin@gmail.com>
2020-03-08 18:16:06 +0100

    dynamic navbar login info and creation of signout page and script

--- src/includes/topmenu.php
+++ src/includes/topmenu.php
@@ -4,6 +4,10 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="navbar-collapse collapse justify-content-stretch" id="navbar7">
+
+        <?php
+
+    if (empty($_SESSION)): ?>
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
                 <a class="nav-link btn btn-outline-secondary" href="#">Sign Up</a>
@@ -12,8 +16,24 @@
             &nbsp;
 
             <li class="nav-item">
-                <a class="nav-link nav-link btn btn-primary text-white" href="#">Sign In</a>
+                <a class="nav-link btn btn-primary text-white" href="#">Sign In</a>
             </li>
         </ul>
+        <?php
+        else : ?>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="#" class="nav-link"><?php echo $_SESSION["nickname"]; ?></a> <!-- replace # by profil link -->
+            </li>
+            <li class="nav-item">
+                <a href="signout.php" class="nav-link">Sign Out</a> <!-- replace # by profil link -->
+            </li>
+        </ul>
+        <?php endif;?>
+
+
+
+
+
     </div>
 </nav>
