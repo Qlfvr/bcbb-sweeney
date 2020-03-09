97dbdccc6d230097be36ecf364ad47f864ec1722
SarahG4000 <sarah.guillaume4000@gmail.com>
2020-03-09 17:28:09 +0100

    theme presque en fin et debut du crud

---
+++ src/profil.php
@@ -0,0 +1,34 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Profil</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/a990d1fe00.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="/css/style.css" />
+  <link rel="stylesheet" href="/css/stylesg.css" />
+
+</head>
+
+<body>
+
+  <?php 
+include "includes/topmenu.php";
+?>
+  <div class="wrapper">
+
+    <?php include "includes/sidebar.php";?>
+
+</div>
+
+<body>
+
+<?php include "includes/contenuprofil.php"?>
+
+
+
+</body>
+
+</html>
