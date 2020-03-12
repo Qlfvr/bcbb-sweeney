f9bb1669014d086aef56fb1b04b0ec5db9cdd175
SarahG4000 <sarah.guillaume4000@gmail.com>
2020-03-12 16:35:25 +0100

    verification email entamer

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://kit.fontawesome.com/a990d1fe00.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="/css/style.css" />
-  <title>Document</title>
+  <title>Sweeney</title>
 </head>
 
 <body>
